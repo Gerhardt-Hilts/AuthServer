b60dc826a1a330393751b40176026cbbf82b6b7a
Gerhardt Hilts <gerhardthilts@gmail.com>
2019-07-31 17:11:49 -0700

    added identity server to startup configuration

---
+++ Auth/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Auth.Layers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+        
+        public Startup(IHostingEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddInMemoryIdentityResources(AuthConfig.GetIdentityResources())
+                .AddInMemoryApiResources(AuthConfig.GetApis())
+                .AddInMemoryClients(AuthConfig.GetClients())
+                .AddTestUsers(AuthConfig.GetUsers());
+            services.AddIdentityServer();
+            
+            var builder = services;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
