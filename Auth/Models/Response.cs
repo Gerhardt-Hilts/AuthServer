bbf961421cbc464d9694130efe270a4ba19dc723
Gerhardt Hilts <gerhardthilts@gmail.com>
2019-08-15 03:57:06 -0700

    added response type

---
+++ Auth/Models/Response.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Auth.Models
+{
+    public class Response<TDataType>
+    {
+        // Status Sub Class
+        public class ResponseStatus
+        {
+            // Status Code Enum
+            public enum ResponseStatusCode
+            {
+                Ok = 200,
+                BadRequest = 400,
+                Unauthorized = 401,
+                NotFound = 404
+            }
+
+            // dictionary tying status codes enum to strings for ease
+            private readonly Dictionary<ResponseStatusCode, string> _statusTypes =
+                new Dictionary<ResponseStatusCode, string>
+                {
+                    {ResponseStatusCode.Ok, "success"},
+                    {ResponseStatusCode.BadRequest, "bad request"},
+                    {ResponseStatusCode.Unauthorized, "unauthorized"},
+                    {ResponseStatusCode.NotFound, "not found"}
+                };
+
+            // Properties
+            public bool Error;
+            public ResponseStatusCode Code;
+            public string Type;
+            public string Message;
+
+            public ResponseStatus(ResponseStatusCode code, string message)
+            {
+                Error = code == ResponseStatusCode.Ok;
+                Code = code;
+                Type = _statusTypes[code];
+                Message = message;
+            }
+
+        }
+
+        public ResponseStatus Status;
+        public TDataType Data;
+
+        // constructor
+        public Response(ResponseStatus.ResponseStatusCode code, string message, TDataType dataPayload)
+        {
+            Status = new ResponseStatus(code, message);
+            Data = dataPayload;
+        }
+    }
+}
