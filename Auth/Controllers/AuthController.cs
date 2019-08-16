2505ba83d067cf820730b6105c90781467f79350
Gerhardt Hilts <gerhardthilts@gmail.com>
2019-08-16 01:11:40 -0700

    modified controller for auth management

---
+++ Auth/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Auth.Logic;
+using Auth.Models;
+using Auth.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly TokenLogic _tokenLogicLayer = new TokenLogic("secret");
+        
+        // POST auth/login-user
+        [HttpPost("login-user")]
+        public Response<string> LoginUser(UserCredentials userLoginCredentials)
+        {
+            var username = userLoginCredentials.Username;
+            var password = userLoginCredentials.Password;
+            // check for valid username and password
+            var userAuthenticated = UserLogic.AuthenticateUser(username, password);
+            // respond if bad request
+            if (!userAuthenticated)
+                return new Response<string>(Response<string>.ResponseStatus.ResponseStatusCode.BadRequest,
+                    "user could not be authenticated", "");
+            // get user id
+            var user = UserLogic.GetUserByUsername(username);
+            // create access token with user id as subject
+            var accessToken = _tokenLogicLayer.GenerateTokensForUser(user.UserId);
+            // return access token in response
+            return new Response<string>(Response<string>.ResponseStatus.ResponseStatusCode.Ok, "user was authenticated", accessToken);
+        }
+        
+        // GET auth/test
+        // basic get method for testing
+        [HttpGet("test")]
+        public Response<string> Test()
+        {
+            Console.WriteLine("TESTING!");
+            return new Response<string>(Response<string>.ResponseStatus.ResponseStatusCode.Ok, "test success", "success!");
+        }
+    }
+}
