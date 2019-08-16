3615212f44400ce865291beeebb877e13e6cd716
Gerhardt Hilts <gerhardthilts@gmail.com>
2019-08-16 00:55:13 -0700

    added user class

---
+++ Auth/Models/User.cs
@@ -0,0 +1,12 @@
+namespace Auth.Models
+{
+    public class User
+    {
+        public string Username;
+        public string Email;
+        public string TimeCreatedAt;
+        public string EmailVerified;
+        public string Phone;
+        public string UserId;
+    }
+}
