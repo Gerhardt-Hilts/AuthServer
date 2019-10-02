2520ad9062005dbae70733c07523950796e9da1f
Gerhardt Hilts <gerhardthilts@gmail.com>
2019-10-01 21:55:42 -0700

    removed test method

--- Auth/Controllers/AuthController.cs
+++ Auth/Controllers/AuthController.cs
@@ -69,14 +69,6 @@ namespace Auth.Controllers
             }
         }
 
-        // GET auth/test
-        // basic get method for testing
-        [HttpGet("test")]
-        public HttpsResponse<string> Test()
-        {
-            Console.WriteLine("TESTING!");
-            return new HttpsResponse<string>(HttpsResponse<string>.ResponseStatus.ResponseStatusCode.Ok, "test success", "success!");
-        }
         
         // who has the right to access this auth server?
         // catalogue ids
