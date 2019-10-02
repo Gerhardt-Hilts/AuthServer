3e3c3337da807b753694ccb51ca78e59c2f4d4e2
Gerhardt Hilts <gerhardthilts@gmail.com>
2019-10-01 21:59:21 -0700

    changed AuthTokens Struct to Class

--- Auth/Models/AuthTokens.cs
+++ Auth/Models/AuthTokens.cs
@@ -2,7 +2,7 @@ using Microsoft.Win32.SafeHandles;
 
 namespace Auth.Models
 {
-    public struct AuthTokens
+    public class AuthTokens
     {
         public string accessToken;
         public string refreshToken;
