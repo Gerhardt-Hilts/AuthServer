297a2753e1ae87fc1e47ea4fc9d4f2a148d2bfdb
Gerhardt Hilts <gerhardthilts@gmail.com>
2019-10-01 21:59:01 -0700

    added test method to test controller

--- Auth/Controllers/TestController.cs
+++ Auth/Controllers/TestController.cs
@@ -1,3 +1,5 @@
+using System;
+using Auth.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Auth.Controllers
@@ -6,6 +8,13 @@ namespace Auth.Controllers
     [ApiController] 
     public class TestController : ControllerBase
     {
-        
+        // GET auth/test
+        // basic get method for testing
+        [HttpGet("test")]
+        public HttpsResponse<string> Test()
+        {
+            Console.WriteLine("TESTING!");
+            return new HttpsResponse<string>(HttpsResponse<string>.ResponseStatus.ResponseStatusCode.Ok, "test success", "success!");
+        }
     }
 }
