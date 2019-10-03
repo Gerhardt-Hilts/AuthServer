dc648d94632814d5baa9a9bc910245b0b32c7e1e
Gerhardt Hilts <gerhardthilts@gmail.com>
2019-10-02 23:37:21 -0700

    reordered properties and added constructor

--- Auth/Models/AccessToken.cs
+++ Auth/Models/AccessToken.cs
@@ -4,10 +4,21 @@ namespace Auth.Models
     {
         public string Id;
         public string LiteralToken;
+        public string UserId;
+        public string ClientId;
+        public string ScopeId;
         public long IssuedAt;
         public long ExpiresAt;
-        public string ClientId;
-        public string UserId;
-        public string ScopeId; 
+
+        public AccessToken(string id, string literalToken, string userId, string clientId, string scopeId, long issuedAt, long expiresAt)
+        {
+            Id = id;
+            LiteralToken = literalToken;
+            UserId = userId;
+            ClientId = clientId;
+            ScopeId = scopeId;
+            IssuedAt = issuedAt;
+            ExpiresAt = expiresAt;
+        }
     }
 }
